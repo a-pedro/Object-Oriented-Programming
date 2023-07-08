cb667bd2c38ff87202705b235519651560c0a569
a-pedro <116759801+a-pedro@users.noreply.github.com>
2023-07-08 11:00:47 +0200

    Add files via upload

---
+++ extend-constructors-to-receive-arguments.js
@@ -0,0 +1,10 @@
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+
+} let terrier = new Dog("bill", "white");
+let pitbull = new Dog("rock", "red");
+
+console.log(terrier);
+console.log(pitbull);
