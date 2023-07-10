f2ab74bbdb8f02ff9d4dece7520e2aceb1677366
a-pedro <116759801+a-pedro@users.noreply.github.com>
2023-07-10 17:43:18 +0200

    Add files via upload

---
+++ use-inheritance-so-you-dont-repeat-yourself.js
@@ -0,0 +1,36 @@
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+eat: function() {
+    console.log("nom nom nom");
+  }
+};
+Cat.prototype = {
+  constructor: Cat
+};
+Bear.prototype = {
+  constructor: Bear
+};
